62f081724e63e40b5bdd49f2312c347f2aced8db
오은진 <oeunj08@naver.com>
2022-03-23 22:31:32 +0900

    JPA 실전 예제2

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -17,6 +20,19 @@ public class JpaMain {
         tx.begin();
 
         try{
+            // 양방향 연관관계인 경우 (시작)
+//            Order order = new Order();
+//            order.addOrderItem(new OrderItem());
+            // 양방향 연관관계 끝
+
+            // 단방향 연관관계인 경우 (시작)
+            Order order = new Order();
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            em.persist(orderItem);
+            // 단방향 연관관계 끝
 
             tx.commit();
         } catch (Exception e) {
